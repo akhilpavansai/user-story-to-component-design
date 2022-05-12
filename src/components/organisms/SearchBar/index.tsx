9188336d74ad44b6aa0ba74745136dbfe1bfe541
akhilpavansai <akhilpavasai.machavaram@zemosolabs.com>
2022-05-12 15:54:57 +0530

    updated components markup

--- src/components/organisms/SearchBar/index.tsx
+++ src/components/organisms/SearchBar/index.tsx
@@ -1,14 +1,39 @@
-import { styled, TextField } from "@mui/material";
+import { InputAdornment, styled, TextField } from "@mui/material";
+import Image from "../../atoms/Image";
 import React from "react";
 
 const PrimarySearchBar = styled(TextField)({});
 const SecondarySearchBar = styled(TextField)({});
 
-const SearchBar = ({ variant }) => {
+const SearchBar = ({ variant, onchange }) => {
   if (variant === "primary") {
-    return <PrimarySearchBar />;
+    return (
+      <PrimarySearchBar
+        onChange={onchange}
+        placeholder="search fot titles and authors"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Image imgSrc={"search.png"} />
+            </InputAdornment>
+          ),
+        }}
+      />
+    );
   } else if (variant === "secondary") {
-    return <SecondarySearchBar />;
+    return (
+      <SecondarySearchBar
+        placeholder="search for title or author"
+        onChange={onchange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Image imgSrc={"search.png"} />
+            </InputAdornment>
+          ),
+        }}
+      />
+    );
   }
   return <div>SearchBar</div>;
 };
